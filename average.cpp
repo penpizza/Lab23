b3a67bcc18c6bcd4a43844ca64488c37dd781662
penpizza <110964198+penpizza@users.noreply.github.com>
2023-03-04 20:16:46 +0700

    completed

--- average.cpp
+++ average.cpp
@@ -1,1 +1,17 @@
+#include<iostream>
+using namespace std;
 
+int main (int argc,char *argv[]){
+    int a,sum=0;
+    if(argc ==1){
+        cout << "Please input numbers to find average.";
+    }else {
+        cout << "---------------------------------\n";
+        for(int i=0; i<argc; i++){
+            a = atoi(argv[i]);
+            sum +=a;
+        }
+        cout << "Average of "<<argc-1<<" numbers = "<< sum/(argc-1)<<endl;
+        cout << "---------------------------------\n";
+    }
+}
